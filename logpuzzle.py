849145317a01e43705eadf551771c7079f283968
Ankit Sharma <godotcoffee231@gmail.com>
2018-12-22 20:38:06 +0530

    Comments

--- logpuzzle.py
+++ logpuzzle.py
@@ -20,50 +20,50 @@ Here's what a puzzle url looks like:
 
 
 def read_urls(filename):
-  myFile=open(filename,"r")
-  contents=myFile.readlines()
+  myFile=open(filename,"r") ##opening the provided file in read mode
+  contents=myFile.readlines() ## saving the contents of the file in a list called contents
   #print(contents)
-  urls=[]
-  for s in contents:
-      string=s
-      pat=r'/edu.*.jpg'
-      match = re.search(pat,string)
-      if match:
+  urls=[] ##creating a empty list that'll haev all the complete urls
+  for s in contents: ##Now iterate the contents list
+      string=s ## current string is saved in a variable string
+      pat=r'/edu.*.jpg' ##The pattern to match the image
+      match = re.search(pat,string) ##matching with regex
+      if match: ##If image exists in current string
           print("Matched")
          # print(match.group())
-          urls.append("http://code.google.com"+match.group())
+          urls.append("http://code.google.com"+match.group()) ##insert in urls list the complete url obtained from current string
       else:
-          print("Failed")
-  myFile.close()
-  return urls
+          print("Failed") ##else do nothing
+  myFile.close() ##close the file to transfer the data from buffer to file
+  return urls ##return the urls list so as we can pass this to other function
   
 
   
 
 def download_images(img_urls, dest_dir):
-    counter=1
-    filenames=[]
-    for u in img_urls:
+    counter=1 ## counter to show progress
+    filenames=[] ## here we save filenames in this list
+    for u in img_urls: ##iterate over image urls strings
         cururl=u
         pat=r'puzzle/.*.jpg'
         match=re.search(pat,cururl)
         filename=match.group()
-        filenames.append(filename[7:])
-        print((counter/len(img_urls))*100,"%")
-        urllib.request.urlretrieve(cururl,dest_dir+filename[7:])
+        filenames.append(filename[7:]) #Getting filename to use it for writing 
+        print((counter/len(img_urls))*100,"%") ##showing progress
+        urllib.request.urlretrieve(cururl,dest_dir+filename[7:]) ##downloading file and saving it in folder. The downloaded file is saved by name extraced above
         counter+=1
-    return filenames
+    return filenames ## return the list of file names
   
 
 def main():
-    myUrls=read_urls("./animal_code.google.com")
-    myFileNames=download_images(myUrls,"./downloaded_images/")
-    htmlfile=open("./downloaded_images/image.html","w")
-    myFileNames.sort()
-    htmlfile.write("<html><head><body>")
+    myUrls=read_urls("./animal_code.google.com") ##Passing the log file name. This function returns a list of complete urls+
+    myFileNames=download_images(myUrls,"./downloaded_images/") ## Passing the above url list and folder name
+    htmlfile=open("./downloaded_images/image.html","w") ##open manually created html file
+    myFileNames.sort() ##Sort the list of file names
+    htmlfile.write("<html><head><body>") #write the contents of html
     for i in myFileNames:
-        htmlfile.write("<img src=./"+i+">")
-    htmlfile.write("</body></head></html>")
+        htmlfile.write("<img src=./"+i+">") ##this file is in same directory as downloaded images so ./filname works for src
+    htmlfile.write("</body></head></html>") ##closing tags
     
     
     
